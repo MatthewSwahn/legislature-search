be14e3d8fda103f95f1ce7c80b6a4fa2c520e1b2
Matthew Swahn <matthewswahn@gmail.com>
2025-06-16 22:30:08 -0400

    fix typo

--- EDA/tokens_by_part.py
+++ EDA/tokens_by_part.py
@@ -1,4 +1,4 @@
-from Legislature-Search.parsing.text_by_part import get_text_from_xml
+from parsing.text_by_part import get_text_from_xml
 from lxml import etree
 import pandas as pd
 import sentencepiece as spm
@@ -8,4 +8,4 @@ root = etree.parse('data/congress_119_hr1/BILLS-119hr1eh.xml')
 parts = get_text_from_xml(root=root)
 
 parts_pd = pd.DataFrame(parts)
-print(parts_pd.info())Legi
+print(parts_pd.info())
