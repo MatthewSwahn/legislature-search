932ef6515adc89f0a9ff4a146678dcb6e4944afc
Matthew Swahn <matthewswahn@gmail.com>
2025-06-17 22:00:45 -0400

    rename tokens by part to by paragraph

---
+++ EDA/tokens_by_paragraph.py
@@ -0,0 +1,46 @@
+from lxml import etree
+import pandas as pd
+from transformers import BigBirdTokenizer
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sys
+import os
+sys.path.append(os.getcwd())
+
+import parser
+
+
+# import data
+root = etree.parse('data/congress_119_hr1/BILLS-119hr1eh.xml')
+
+bbb_data = parser.get_xml_text_by_paragraph(root=root)
+
+bbb_df = pd.DataFrame(bbb_data)
+
+
+# instantiate tokenizer. Leaning bigbird as the number of tokens is large
+tokenizer = BigBirdTokenizer.from_pretrained('google/bigbird-roberta-large')
+#print('tok enizer max len', tokenizer.model_max_length) # 4096
+# tokenize text
+encoded = tokenizer(bbb_df.text.to_list(), padding=True, truncation=True, return_tensors='pt')
+
+input_ids_list = encoded['input_ids'].tolist()
+
+# get number of tokens as column
+pad_token_id = tokenizer.pad_token_id
+bbb_df['num_spm_tokens_by_para'] = [
+    sum(token_id != pad_token_id for token_id in input_ids)
+    for input_ids in input_ids_list
+]
+# how much text goes to the 4096 token limit
+bbb_df['max_token_ind'] = bbb_df['num_spm_tokens_by_para'].apply(lambda x: 1 if x == 4096 else 0)
+
+print('how much text hits 4096 tokens?', bbb_df['max_token_ind'].value_counts()) # 7
+
+bbb_df.to_csv('EDA/bbb_df_by_para_v1.csv')
+bbb_df[bbb_df['max_token_ind'] == 1].to_csv('EDA/bbb_df_by_para_max_tokens.csv')
+
+sns.histplot(data=bbb_df, x="num_spm_tokens_by_para", kde=True, bins=30)
+plt.savefig('EDA/spm_token_dist_by_para.png', dpi=300)
+
+# current xml parsing gets text on the section level and all hit 4096 tokens
